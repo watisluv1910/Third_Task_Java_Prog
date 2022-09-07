ee97126d3e0d15fdba501d04ccf5cb655356e319
Vladislav Nasevich <nasevich.vlasd.03@gmail.com>
2022-09-08 00:35:01 +0300

    Completed third subtask.

---
+++ src/com/thirdTask/thirdSubtask/BookTest.java
@@ -0,0 +1,11 @@
+package com.thirdTask.thirdSubtask;
+
+import com.secondTask.thirdSubtask.ThirdSubtask;
+
+public class BookTest {
+
+    public static void main(String[] args) {
+
+        ThirdSubtask.main(new String[]{});
+    }
+}
